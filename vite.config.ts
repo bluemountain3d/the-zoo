2b17c7e5d6727e919908531080f12bf6a6b16431
Egil Eskilsson <egil@egileskilsson.se>
2025-08-25 11:20:48 +0200

    feat(scss): set up the base of a modern and scalable SCSS framework
    - Installed the sass-embedded package
    - Created a modular SCSS folder structure with abstracts, base, and utilities.
    - Configured Vite to use @use for global access to SCSS functions and variables.
    - Included a base CSS reset and core utilities for consistency and responsive layouts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "/src/scss/abstracts/functions" as fn;
+          @use "/src/scss/abstracts/variables" as var;
+          @use "/src/scss/abstracts/mixins" as mx;
+        `
+      }
+    }
+  }
 })
