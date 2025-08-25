7f9f52302375f123a760e7723b102309495891bf
Egil Eskilsson <egil@egileskilsson.se>
2025-08-25 14:54:22 +0200

    feat(main-header): add basic header and navigation
    This commit introduces the main header component with brand text and a navigation menu. It includes the basic styling for the new component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,24 @@
+import { NavLink } from 'react-router';
 import './Header.scss';
 
 export const Header = () => {
   return (
-    <header>
-      <p>Header</p>
+    <header className="header">
+      <div className="container-boxed header__inner">
+        <div className="header__brand">
+          THE ZOO
+        </div>
+        <nav className="header__nav">
+          <ul className="header__nav-menu">
+            <li className="header__nav-item">
+              <NavLink to="/" className="header__nav-link">Hem</NavLink>
+            </li>
+            <li className="header__nav-item">
+              <NavLink to="/animals" className="header__nav-link">Våra djur</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
