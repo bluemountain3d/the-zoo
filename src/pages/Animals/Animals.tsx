b037fc63bd77abbc7d2ac0e5177b13b47f7a3550
Egil Eskilsson <egil@egileskilsson.se>
2025-08-26 11:47:31 +0200

    feat(animal-presentation): create basic animal list presentation
    - Use data from router loader for animal list
    - Add basic layout and placeholder images
    - Fix container styling

--- src/pages/Animals/Animals.tsx
+++ src/pages/Animals/Animals.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
 import './Animals.scss';
 import type { AnimalsLoader } from '../../loaders/animalsLoader';
 
@@ -6,24 +6,45 @@ export const Animals = () => {
   const { animals } = useLoaderData() as AnimalsLoader
 
   return (
-    <section>
-      <h1>Våra djur</h1>
-      <div>
-        {animals.map((a) => (
-          <article key={a.id}>
-            <picture>
-              <img 
-                src={a.imageUrl} 
-                alt={`Bid på ${a.name} (${a.latinName})`}
-                width="256"
-              />
-            </picture>
-            <hgroup>
-              <h2>{a.name}</h2>
-              <p>({a.latinName})</p>
-            </hgroup>
-          </article>
-        ))}
+    <section className="animals">
+      <div className="animals__heading container-boxed">
+        <h1 className='animals__title'>Våra djur</h1>
+      </div>
+      <div className="container-boxed animals__inner">
+        <ul className="animals__list">
+          {animals.map((a) => (
+            <li key={a.id} className="animals__item">
+              <Link to={`/animal/${a.id}`} className='animals__image-link'>
+                <picture className='animals__image-wrapper'>
+                  <img 
+                    src={a.imageUrl} 
+                    alt={`Bid på ${a.name} (${a.latinName})`}
+                    className='animals__image'
+                    onError={(e) => {
+                      e.currentTarget.src = '/placeholder.jpeg'
+                    }}
+                    width="256"
+                  />
+                </picture>
+              </Link>
+              <div className='animals__details'>
+                <hgroup className='animals__item-heading'>
+                  <h2 className='animals__item-title'>{a.name}</h2>
+                  <p className='animals__item-subtitle'>({a.latinName})</p>
+                </hgroup>
+                <p className='animals__item-description'>{a.shortDescription}</p> 
+                <div className='animals__item-status-bar'>
+                  <div className="animals__item-status">
+                    Här ska status om det är dags att mata stackarn vara
+                  </div>
+                </div>
+                <Link to={`/animal/${a.id}`} className='animals__go-to-btn'>
+                    Visa mer information om djuret
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
