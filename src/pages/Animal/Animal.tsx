193a6f821c03dca7395a368b63d9b23f14e4139c
Egil Eskilsson <egil@egileskilsson.se>
2025-08-27 08:47:39 +0200

    feat(animal): improve layout and styling for Animal component details

--- src/pages/Animal/Animal.tsx
+++ src/pages/Animal/Animal.tsx
@@ -13,6 +13,23 @@ export const Animal = () => {
     return <p>Tyvärr! Hittar inte djuret. Det kanske sover...</p>
   }
 
+  const dateTransform = (dateStr) => {
+    const date = new Date(dateStr);
+
+    return {
+      date: date, // För tidsberäkningar
+      display: date.toLocaleString('sv-SE', {
+        year: 'numeric',
+        month: '2-digit', 
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+      }) // "2021-04-15 14:09"
+    };
+  }
+
+
+
   return (
     <section className='animal'>
       <div className="container-boxed animal__inner">
@@ -24,15 +41,24 @@ export const Animal = () => {
         <div className="animal__details">
           <hgroup className='animal__heading'>
             <h2 className='animal__title'>{animal.name}</h2>
-            <p className='animal__subtitle'>{animal.latinName}</p>
+            <p className='animal__subtitle'>({animal.latinName})</p>
           </hgroup>
           <p className="animal__description">{animal.longDescription}</p>
-          <div className="animal__status">
-            <div className="animal__last-fed">
+          <div className="animal__status-wrapper">
+            <div className="animal__status">
               {/* 
                 - När ett djur inte har fått mat på tre timmar skall en indikation på att djuret snart behöver matas visas.
               */}
-              {animal.lastFed}
+              <div className="animal__feed-status">
+                <p className="animal__status-label">Senast matad:</p>
+                <p className="animal__last-fed">{dateTransform(animal.lastFed).display}</p>
+                <p className="animal__status-message">{/* Meddelande från logik */} Statusmeddelande</p>
+              </div>
+              <div className="animal__feed-status">
+                <p className="animal__status-label">Kan matas igen:</p>
+                <p className="animal__last-fed">{dateTransform(animal.lastFed).display}</p>
+                <p className="animal__status-message">{/* Meddelande från logik */} Statusmeddelande</p>
+              </div>            
             </div>
             <button 
               className="animal__feed-btn"
@@ -43,8 +69,8 @@ export const Animal = () => {
               */}
               Mata
             </button>
-            
           </div>
+          
         </div>
       </div>
     </section>
