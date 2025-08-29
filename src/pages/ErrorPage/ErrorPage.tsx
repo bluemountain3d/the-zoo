08377f321596be995f6b2ea3a0c5b00aae84c8e5
Egil Eskilsson <egil@egileskilsson.se>
2025-08-29 16:25:03 +0200

    feat(error-page): enhance ErrorPage component with styling and create corresponding SCSS file

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -4,8 +4,8 @@ export const ErrorPage = () => {
   // const error = useRouteError();
 
   return (
-    <section className='error-page'>
-      <h1 className="error-page__title">Oops! Något gick fel</h1>
+    <section className='error-page page-section'>
+      <h1 className="error-page__title heading-md">Oops! Något gick fel</h1>
       <p className="error-page__message">Ett fel inträffade när sidan skulle laddas</p>
       <a href="/" className="error-page__link">Tillbaka till startsidan</a>
     </section>
