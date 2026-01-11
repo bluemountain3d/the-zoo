65d9c4d2814fc93bb1d89e623648b87db5b25cdf
Egil Eskilsson <egil@egileskilsson.se>
2026-01-11 11:50:06 +0100

    fix(): change url for placeholder images

--- src/pages/Animal/Animal.tsx
+++ src/pages/Animal/Animal.tsx
@@ -13,27 +13,29 @@ export const Animal = () => {
 
   // Null-check för att se om context existerar.
   if (!context) {
-    return <ErrorMessage message="Ett fel uppstod: data kunde inte laddas." />;
+    return <ErrorMessage message='Ett fel uppstod: data kunde inte laddas.' />;
   }
 
   // Check för att kolla så id inte är undefined
   if (!id) {
-    return <ErrorMessage message="Inget djur specificerat." />;
+    return <ErrorMessage message='Inget djur specificerat.' />;
   }
 
   // Destrukturera om context finns
   const { animals, dispatch } = context;
 
   // Hitta enskilt djur från listan med djur med id från params
-  const animal = animals.find(a => a.id === +(id || '-1'));
+  const animal = animals.find((a) => a.id === +(id || '-1'));
 
-  // Check om jur finns 
+  // Check om jur finns
   if (!animal) {
-    return <ErrorMessage 
-      message="Tyvärr! Hittar inte djuret. Det kanske sover..." 
-      linkText="Gå tillbaka till våra djur"
-      linkUrl="/animals"
-    />;
+    return (
+      <ErrorMessage
+        message='Tyvärr! Hittar inte djuret. Det kanske sover...'
+        linkText='Gå tillbaka till våra djur'
+        linkUrl='/animals'
+      />
+    );
   }
 
   const feedingStatus = getFeedingStatus(animal.lastFed, 'detail');
@@ -42,65 +44,73 @@ export const Animal = () => {
     if (animal && feedingStatus.canFeed) {
       dispatch({
         type: AnimalActionType.feedAnimal,
-        payload: animal.id.toString()
+        payload: animal.id.toString(),
       });
     }
-  }
+  };
 
   return (
     <section className='animal page-section'>
-
-      <div className="animal__hero">
-        <div className="animal__hero-content container-boxed">
-          <hgroup className="animal__heading heading-group-xl">
-            <h1 className="animal__title heading-group__title">{formatNamePossession(animal.name)} sida</h1>
+      <div className='animal__hero'>
+        <div className='animal__hero-content container-boxed'>
+          <hgroup className='animal__heading heading-group-xl'>
+            <h1 className='animal__title heading-group__title'>
+              {formatNamePossession(animal.name)} sida
+            </h1>
             {/* <p className="animal__subtitle heading-group__subtitle">({animal.latinName})</p> */}
           </hgroup>
         </div>
       </div>
 
-      <div className="container-boxed animal__inner">
-        <div className="animal__presentation">
-          <div className="animal__image-wrapper">
+      <div className='container-boxed animal__inner'>
+        <div className='animal__presentation'>
+          <div className='animal__image-wrapper'>
             <picture>
-              <img 
-                src={animal.imageUrl} 
-                alt={`Bild på djuret ${animal.name}`} 
-                className="animal__image" 
+              <img
+                src={animal.imageUrl}
+                alt={`Bild på djuret ${animal.name}`}
+                className='animal__image'
                 onError={(e) => {
-                  e.currentTarget.src = '/placeholder.avif'
+                  e.currentTarget.src =
+                    import.meta.env.BASE_URL + 'placeholder.avif';
                 }}
               />
             </picture>
           </div>
-          
-          <div className="animal__details">
-            <p className="animal__label">Fakta om  {animal.name}:</p>
-            <p className="animal__description">{animal.longDescription}</p>
-            <div className="animal__status-wrapper">
-              <div className="animal__status">
-                <div className={`animal__last-fed ${feedingStatus.statusClass}`}>
-                  <p className="animal__label">Senast matad:</p>
-                  <p className="animal__time">{feedingStatus.lastFedFormatted}</p>
-                  <p className={`animal__status-message ${feedingStatus.statusClass}`}>
+
+          <div className='animal__details'>
+            <p className='animal__label'>Fakta om {animal.name}:</p>
+            <p className='animal__description'>{animal.longDescription}</p>
+            <div className='animal__status-wrapper'>
+              <div className='animal__status'>
+                <div
+                  className={`animal__last-fed ${feedingStatus.statusClass}`}>
+                  <p className='animal__label'>Senast matad:</p>
+                  <p className='animal__time'>
+                    {feedingStatus.lastFedFormatted}
+                  </p>
+                  <p
+                    className={`animal__status-message ${feedingStatus.statusClass}`}>
                     {`${animal.name} ${feedingStatus.statusMessage}`}
                   </p>
                 </div>
 
-                <div className={`animal__next-feed ${feedingStatus.statusClass}`}>
-                  <p className="animal__label">Kan matas igen:</p>
-                  <p className="animal__time">{feedingStatus.canFeedAgain}</p>
-                  {feedingStatus.canFeed ? `Nu kan ${animal.name} få mat igen` : ""}
-                </div>            
+                <div
+                  className={`animal__next-feed ${feedingStatus.statusClass}`}>
+                  <p className='animal__label'>Kan matas igen:</p>
+                  <p className='animal__time'>{feedingStatus.canFeedAgain}</p>
+                  {feedingStatus.canFeed
+                    ? `Nu kan ${animal.name} få mat igen`
+                    : ''}
+                </div>
               </div>
-              
-              <Button 
-                variant='primary' 
+
+              <Button
+                variant='primary'
                 className={`animal__feed-btn ${feedingStatus.statusClass}`}
                 onClick={handleFeed}
                 fullWidth={true}
-                disabled={!feedingStatus.canFeed}
-              >
+                disabled={!feedingStatus.canFeed}>
                 Ge {animal.name} mat!
               </Button>
             </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router";
-import { ErrorMessage } from "../../components/ErrorMessage/ErrorMessage";
-import { useContext } from "react";
-import { AnimalsContext } from "../../contexts/animalsContext";
-import { Button } from "../../components/Button";
+import { Link } from 'react-router';
+import { ErrorMessage } from '../../components/ErrorMessage/ErrorMessage';
+import { useContext } from 'react';
+import { AnimalsContext } from '../../contexts/animalsContext';
+import { Button } from '../../components/Button';
 
 export const Home = () => {
   const context = useContext(AnimalsContext);
 
   // Null-check för att se om context existerar.
   if (!context) {
-    return <ErrorMessage message="Ett fel uppstod: data kunde inte laddas."/>;
+    return <ErrorMessage message='Ett fel uppstod: data kunde inte laddas.' />;
   }
 
   // Destrukturera om context finns
@@ -21,44 +21,51 @@ export const Home = () => {
     .slice(0, 3);
 
   return (
-    <section className="page-section home">
-      <div className="home__hero">
-        <div className="container-boxed home__hero-content">
-          <hgroup className="home__hero-heading heading-group-xl">
-            <h1 className="heading-group__title">Välkommen till Vårt Zoo</h1>
-            <p className="heading-group__subtitle">Upptäck fantastiska djur från hela världen</p>
+    <section className='page-section home'>
+      <div className='home__hero'>
+        <div className='container-boxed home__hero-content'>
+          <hgroup className='home__hero-heading heading-group-xl'>
+            <h1 className='heading-group__title'>Välkommen till Vårt Zoo</h1>
+            <p className='heading-group__subtitle'>
+              Upptäck fantastiska djur från hela världen
+            </p>
           </hgroup>
-          <Button to="/animals" variant="overlay" className="home__cta-button">
+          <Button to='/animals' variant='overlay' className='home__cta-button'>
             Se våra djur
           </Button>
         </div>
       </div>
 
-      <div className="container-boxed home__highlight">
-        <h2 className="heading-lg home__highlight-heading">Populära djur</h2>
-        <div className="home__featured-animals">
+      <div className='container-boxed home__highlight'>
+        <h2 className='heading-lg home__highlight-heading'>Populära djur</h2>
+        <div className='home__featured-animals'>
           {featuredAnimals.map((animal) => (
-          <Link to={`/animal/${animal.id}` } key={animal.id}>
-            <article className="featured-animal">
-              <div className="featured-animal__image-wrapper">
-                <picture>
-                  <img 
-                    src={animal.imageUrl} 
-                    alt={`Bild på djuret ${animal.name}`}
-                    className="featured-animal__image"
-                    onError={(e) => {
-                      e.currentTarget.src = '/placeholder.avif'
-                    }} 
-                  />
-                </picture>
-              </div>
-              <hgroup className="featured-animal__heading heading-group-sm">
-                <h3 className="featured-animal__title heading-group__title">{animal.name}</h3>
-                <p className="featured-animal__subtitle heading-group__subtitle">({animal.latinName})</p>
-              </hgroup>
-            </article>
-          </Link>
-        ))}
+            <Link to={`/animal/${animal.id}`} key={animal.id}>
+              <article className='featured-animal'>
+                <div className='featured-animal__image-wrapper'>
+                  <picture>
+                    <img
+                      src={animal.imageUrl}
+                      alt={`Bild på djuret ${animal.name}`}
+                      className='featured-animal__image'
+                      onError={(e) => {
+                        e.currentTarget.src =
+                          import.meta.env.BASE_URL + 'placeholder.avif';
+                      }}
+                    />
+                  </picture>
+                </div>
+                <hgroup className='featured-animal__heading heading-group-sm'>
+                  <h3 className='featured-animal__title heading-group__title'>
+                    {animal.name}
+                  </h3>
+                  <p className='featured-animal__subtitle heading-group__subtitle'>
+                    ({animal.latinName})
+                  </p>
+                </hgroup>
+              </article>
+            </Link>
+          ))}
         </div>
       </div>
     </section>
